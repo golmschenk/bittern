b22e184708128c22410ccb52e4bca821c06ae0b8
golmschenk <greg@olmschenk.com>
2026-01-30 16:40:16 -0500

    Initial commit

---
+++ test/bittern.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Bittern from '../lib/bittern-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bittern-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Bittern.BitternStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
