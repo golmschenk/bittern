15e294073b4c3487fb71c7bcd810e38ffedc0dbb
golmschenk <greg@olmschenk.com>
2026-03-21 11:43:54 -0400

    Add SFTP hosting stack

--- bin/bittern.ts
+++ bin/bittern.ts
@@ -4,6 +4,7 @@ import {LambdaFunctionStack} from '../infrastructure/lambda-function-stack';
 import {ContainerStack} from "../infrastructure/container-stack";
 import {DataHostingStack} from "../infrastructure/data-hosting-stack";
 import {getUsernamesAndSshKeysRecord} from "../infrastructure/ssh-users";
+import {SftpEndpointStack} from "../infrastructure/sftp-endpoint-stack";
 
 const app = new cdk.App();
 
@@ -15,9 +16,15 @@ new ContainerStack(app, 'container-stack', {
     env: {account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION},
 });
 
-new DataHostingStack(app, 'variable-star-dataset-data-hosting-stack', {
+const variableStarDatasetDataHostingStack = new DataHostingStack(app, 'variable-star-dataset-data-hosting-stack', {
     env: {account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION},
     dataName: 'variable-star-dataset',
+    tags: {'working-group': '6', 'deployment-environment': 'production'}
+});
+
+new SftpEndpointStack(app, 'sftp-endpoint-stack', {
+    env: {account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION},
+    buckets: [variableStarDatasetDataHostingStack.bucket],
+    usernameAndSshKeyRecord: getUsernamesAndSshKeysRecord(['golmschenk']),
     tags: {'working-group': '6', 'deployment-environment': 'production'},
-    usernameAndSshKeyRecord: getUsernamesAndSshKeysRecord(['golmschenk'])
 });
