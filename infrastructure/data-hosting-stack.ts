8acab56cf962d7e8a4280e50d2f43c5a8a9e5e4b
golmschenk <greg@olmschenk.com>
2026-04-01 16:17:51 -0400

    Update requester pays method

--- infrastructure/data-hosting-stack.ts
+++ infrastructure/data-hosting-stack.ts
@@ -1,5 +1,6 @@
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cr from 'aws-cdk-lib/custom-resources';
 import {Construct} from 'constructs';
 import {BitternBaseStack, BitternBaseStackProps} from './bittern-base-stack';
 
@@ -17,16 +18,26 @@ export class DataHostingStack extends BitternBaseStack {
             blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS_ONLY,
         });
 
-        const cfnBucket = this.bucket.node.defaultChild as s3.CfnBucket;
-        cfnBucket.addPropertyOverride('RequestPaymentConfiguration', {
-            Payer: 'Requester',
+        new cr.AwsCustomResource(this, 'RequesterPays', {
+            onCreate: {
+                service: 'S3',
+                action: 'putBucketRequestPayment',
+                parameters: {
+                    Bucket: this.bucket.bucketName,
+                    RequestPaymentConfiguration: {Payer: 'Requester'},
+                },
+                physicalResourceId: cr.PhysicalResourceId.of('RequesterPays'),
+            },
+            policy: cr.AwsCustomResourcePolicy.fromSdkCalls({
+                resources: [this.bucket.bucketArn],
+            }),
         });
 
         this.bucket.addToResourcePolicy(new iam.PolicyStatement({
-            sid: `PublicReadGetObject`,
+            sid: 'PublicReadGetObject',
             effect: iam.Effect.ALLOW,
             principals: [new iam.AnyPrincipal()],
-            actions: [`s3:GetObject`],
+            actions: ['s3:GetObject'],
             resources: [`${this.bucket.bucketArn}/*`],
         }));
     }
