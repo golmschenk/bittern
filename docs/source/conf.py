9a71eab0f82335ad29ecbe3108ba55e1ee7e72b5
golmschenk <greg@olmschenk.com>
2026-05-13 16:38:28 -0400

    Add the project docs and placeholder package from the old tools project

---
+++ docs/source/conf.py
@@ -0,0 +1,24 @@
+import datetime
+
+project = 'RGES-PIT Pipeline Tools'
+now_year = datetime.datetime.now().year
+project_start_year = 2025
+author = 'Greg Olmschenk'
+
+html_title = "RGES-PIT Pipeline Tools"
+project_copyright = (f'{f"{project_start_year} - " if project_start_year != now_year else ""}{now_year}, '
+                     f'{author}')
+
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    'sphinx_copybutton',
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+source_suffix = [".rst", ".md"]
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docs/source/index.md
@@ -0,0 +1,9 @@
+# RGES-PIT pipeline tools
+
+## Table of contents
+```{toctree}
+:maxdepth: 2
+:titlesonly:
+
+smce/index
+```
